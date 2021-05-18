1aa7d352fca2acecd4b5f09785396958e94c610d
Douglas Berkley <douglasmberkley@gmail.com>
2021-05-18 18:44:29 +0900

    added the remaning CRUD routes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,10 +1,11 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
   def index
     @pets = Pet.all
   end
 
   def show
-    @pet = Pet.find(params[:id])
   end
 
   def new
@@ -22,9 +23,29 @@ class PetsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path
+  end
+
   private
 
   def pet_params
     params.require(:pet).permit(:name, :address, :species, :found_on)
+  end
+
+  def set_pet
+    @pet = Pet.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # VERB 'path', to: 'controller#action', as: :prefix
+  root to: 'pets#index'
   get '/pets', to: 'pets#index', as: :pets
   get '/pets/new', to: 'pets#new', as: :new_pet
   post '/pets', to: 'pets#create'
   get '/pets/:id', to: 'pets#show', as: :pet
+  get '/pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch '/pets/:id', to: 'pets#update'
+  delete '/pets/:id', to: 'pets#destroy'
 end
